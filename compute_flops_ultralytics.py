03a26ce58318ba05c8468e7c5a3c3c4b5f1b0338
Simon Perrigault <simonperrigault22@gmail.com>
2026-01-09 15:17:23 +0100

    graph watt et flops

--- compute_flops_ultralytics.py
+++ compute_flops_ultralytics.py
@@ -4,14 +4,24 @@
 # model.info(detailed=True) # Prints GFLOPs, parameters, and layer info
 
 # pip install thop
-import torch
-from thop import profile
-import torchvision.models.detection as models
+# import torch
+# from thop import profile
+# import torchvision.models.detection as models
+#
+# # model = models.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+# model = models.ssdlite320_mobilenet_v3_large(pretrained=True)
+# input = torch.randn(1, 3, 640, 640) # YOLO/Faster R-CNN input size
+# macs, params = profile(model, inputs=(input, ))
+#
+# print(f"Total FLOPs: {2 * macs}") # 1 MAC (Multiply-Accumulate) ≈ 2 FLOPs
+# print(f"total GFLOPs: {2 * macs / 1e9}")
 
-# model = models.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
-model = models.ssdlite320_mobilenet_v3_large(pretrained=True)
-input = torch.randn(1, 3, 640, 640) # YOLO/Faster R-CNN input size
-macs, params = profile(model, inputs=(input, ))
+# pip install onnx-tool
+import onnx_tool
 
-print(f"Total FLOPs: {2 * macs}") # 1 MAC (Multiply-Accumulate) ≈ 2 FLOPs
-print(f"total GFLOPs: {2 * macs / 1e9}")
+# modelpath = 'github-psat/rpi4/yolo5/yolov5n.onnx'
+# modelpath = 'github-psat/rpi4/yolo11/yolo11n-416.onnx'
+modelpath = 'github-psat/rpi4/ssd/ssdlite_mobilenet_v3_large-320.onnx'
+# model_profile will perform shape inference and count MACs/FLOPs
+# onnx_tool.model_profile(modelpath)
+onnx_tool.model_profile(modelpath, mcfg={'constant_folding': False})
